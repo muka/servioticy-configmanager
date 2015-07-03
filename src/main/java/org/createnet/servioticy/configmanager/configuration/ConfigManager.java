9d09a9b1d845a2125ef39e206454f6713e86b420
Luca Capra <luca.capra@gmail.com>
2015-07-03 12:07:11 +0200

    refactor, allow configmanager to serve more use cases

--- src/main/java/org/createnet/servioticy/configmanager/configuration/ConfigManager.java
+++ src/main/java/org/createnet/servioticy/configmanager/configuration/ConfigManager.java
@@ -1,4 +1,4 @@
-/**
+/*
  *
  * Copyright 2014 CREATE-NET
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,11 +24,11 @@ import java.io.InputStream;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.createnet.servioticy.configmanager.beans.Configuration;
+
 import org.createnet.servioticy.configmanager.exception.ConfigPropertyNotFound;
 import org.createnet.servioticy.configmanager.exception.ConfigurationFileNotFound;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
@@ -38,7 +38,9 @@ import org.yaml.snakeyaml.constructor.Constructor;
  */
 public class ConfigManager implements IConfigurationManager, Serializable
 {
-        
+
+    Logger logger = LoggerFactory.getLogger(ConfigManager.class);
+    
     String _defaultSource = "/etc/servioticy";
     String _defaultFile = "config.yml";
 
@@ -48,32 +50,22 @@ public class ConfigManager implements IConfigurationManager, Serializable
     String defaultProperties = "/config.properties";
     String defaultConfiguration = "/default.yml";
 
-    Configuration config;
     static HashMap<String, String> resolvedCache = new HashMap<>();
     
     public static void main(String [] args) throws ConfigPropertyNotFound
     {
 
-        ConfigManager m = ConfigManager.getInstance();
-        String x = m.get("couchbase.public_uris");
+        ConfigManager m = new ConfigManager();
 
-        System.out.println("Loaded config ----------------------------");
-        System.out.println(x);
+//        m.load(Configuration.class);
+//        String x = m.get("couchbase.public_uris");
+
+//        System.out.println("Loaded config ----------------------------");
+//        System.out.println(x);
 
     }
 
-    private static ConfigManager instance;
-
-    public static synchronized ConfigManager getInstance() {
-
-        if(instance == null) {
-            instance = new ConfigManager();
-        }
-
-        return instance;
-    }
-
-    private ConfigManager() {
+    public ConfigManager() {
 
         Properties props = new Properties();
 
@@ -86,7 +78,7 @@ public class ConfigManager implements IConfigurationManager, Serializable
 
         } catch (Exception ex) {
 
-            Logger.getLogger(ConfigManager.class.getName()).log(Level.WARNING, "Cannot read properties file {0}", defaultProperties);
+            logger.warn("Cannot read properties file {0}", defaultProperties);
 
             defaultSource = _defaultSource;
             defaultFile = _defaultFile;
@@ -95,132 +87,46 @@ public class ConfigManager implements IConfigurationManager, Serializable
 
     }
 
-    protected void load() throws ConfigurationFileNotFound
+    public IConfiguration load(Class classType, String[] paths) throws ConfigurationFileNotFound
     {
 
-        InputStream input;
+        InputStream input = null;
+        File file = null;
+        
+        for (String path : paths) {
+            
+            // try to load config specified configuration
+            logger.info("Load configuration, checking {0}", path);
+            
+            file = new File(path);
+            input = this.tryLoad(file);
 
-        // try to load config specified configuration
-        // eg. public-api.config.yml
-        Logger.getLogger(ConfigManager.class.getName()).log(Level.INFO, "Load configuration, checking {0}", getFilePath());
-        input = this.tryLoad(getFilePath());
+            if(input != null) {
+                break;
+            }
 
-        if(input == null) {
-            String defConf = _defaultSource + "/" + _defaultFile;
-            Logger.getLogger(ConfigManager.class.getName()).log(Level.INFO, "Trying default configuration {0}", defConf);
-            input = this.tryLoad(defConf);
-        }
-
-        if(input == null) {
-            Logger.getLogger(ConfigManager.class.getName()).log(Level.INFO, "Using jar configuration file {0}", defaultConfiguration);
-            input = ConfigManager.class.getResourceAsStream(defaultConfiguration);
         }
 
         if(input == null) {
             throw new ConfigurationFileNotFound();
         }
 
-        Constructor constructor = new Constructor(Configuration.class);
+        Constructor constructor = new Constructor(classType);
         Yaml yaml = new Yaml(constructor);
+        
+        Configuration conf = (Configuration) yaml.load(input);
+        conf.setConfigurationFile(file);
 
-        this.config = (Configuration) yaml.load(input);
+        return conf;
     }
 
-    protected FileInputStream tryLoad(String filename) {
+    protected FileInputStream tryLoad(File configFile) {
         try {
-            return new FileInputStream(new File(getFilePath()));
+            return new FileInputStream(configFile);
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(ConfigManager.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error(null, ex);
         }
         return null;
     }
 
-    protected String getFilePath() {
-        return defaultSource + "/" + defaultFile;
-    }
-
-    public String getPath() {
-        return defaultSource;
-    }
-
-    public int getOpIdExpiration() {
-        return 5 * 60;
-    }
-    
-    public Configuration getConfiguration() throws ConfigurationFileNotFound
-    {
-
-        if(config == null) {
-            load();
-        }
-
-        return config;
-    }
-
-    public String get(String key)
-    {
-
-        String value = null;
-
-        try {
-            value = getConfiguration().get(key);
-        } catch (ConfigPropertyNotFound ex) {
-            Logger.getLogger(ConfigManager.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ConfigurationFileNotFound ex) {
-            Logger.getLogger(ConfigManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        return value;
-    }
-    
-    public int getInt(String key)
-    {
-        String sval = this.get(key);
-        return (sval != null) ? Integer.parseInt(sval) : null;
-    }
-
-    /**
-     * Resolve a path as specified in `paths` in the configuration file
-     *
-     * @throws ConfigPropertyNotFound
-     * @param key The config key to lookup
-     * @return the FS path
-     */
-    public String getPath(String key) throws ConfigPropertyNotFound
-    {
-
-        if(resolvedCache.containsKey(key)) {
-            return resolvedCache.get(key);
-        }
-
-        String configPath = this.getPath();
-        String name = config.paths.get(key);
-
-        // check if the values specified is a path and exists
-        File file = new File(name);
-        if(file.exists()) {
-
-            String filepath = file.getAbsolutePath();
-            resolvedCache.put(name, filepath);
-
-            return filepath;
-        }
-        else {
-            // check if the path is relative
-            file = new File(configPath + "/" + name);
-
-            if(file.exists()) {
-
-                String filepath = file.getAbsolutePath();
-                resolvedCache.put(name, filepath);
-
-                return filepath;
-            }
-        }
-
-        // defaults to stored name, lookup as jar resource
-        resolvedCache.put(name, name);
-        return name;
-    }    
-    
 }
